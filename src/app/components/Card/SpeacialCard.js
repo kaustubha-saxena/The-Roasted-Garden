8c4fc9427c1579db95b09d94ff33640eccbffb6e
kaustubha-saxena <kaustubhasaxena@gmail.com>
2025-08-02 18:09:49 +0530

    updated

--- src/app/components/Card/SpeacialCard.js
+++ src/app/components/Card/SpeacialCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SpeacialCard = ({item}) => {
   return (
     <>
-    <div className='w-[300px] h-fit overflow-hidden hover:scale-105 hover:transition-transform duration-300 hover:cursor-pointer'>
+    <div className='min-w-[300px] h-fit overflow-hidden hover:scale-105 hover:transition-transform duration-300 hover:cursor-pointer'>
         <img className='min-w-[300px] h-[300px] rounded-xl object-cover' src={`${item.image}`} alt="" />
         <div className='mt-3 px-3'>
 
